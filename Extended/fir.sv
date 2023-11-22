8b41148ec6efcbc30e2b9e6a0bc9643671c8b08d
jonathanfoohy <jonathanfoo28@gmail.com>
2023-11-22 20:07:40 +0800

    Created Extended Folder alongside FIR with parameter

--- Extended/fir.sv
+++ Extended/fir.sv
@@ -1,25 +1,25 @@
 // FIR 16 stages; 16 bit samples. Could be parameterised.
 
-module fir (input logic signed [15:0] in,
+module fir #(parameter N=16)(input logic signed [N-1:0] in,
        input logic input_ready, ck, rst ,
-       output logic signed [15:0] out,
+       output logic signed [N-1:0] out,
        output logic output_ready);
 
-typedef logic signed [15:0] sample_array;
-sample_array samples [0:15];
+typedef logic signed [N-1:0] sample_array;
+sample_array samples [0:N-1];
 
 // generate coefficients from Octave/Matlab
 // disp(sprintf('%d,',round(fir1(15,0.5)*32768)))
 
-const sample_array coefficients [0:15] =
+const sample_array coefficients [0:N-1] =
      '{-81, -134,  318, 645,
      -1257, -2262, 4522, 14633,
      14633, 4522, -2262,-1257,
      645, 318, -134, -81};
 
-logic unsigned [3:0] address; //clog2 of 16 is 4
+logic unsigned [$clog2(N)-1:0] address; //clog2 of 16 is 4
 
-logic signed [31:0] sum;
+logic signed [(N*2)-1:0] sum;
 
 typedef enum logic [1:0] {waiting, loading, processing, saving} state_type;
 state_type state, next_state;
@@ -44,7 +44,7 @@ always_ff @(posedge ck)
     
 always_ff @(posedge ck)
   if (output_ready)
-    out <= sum[30:15];
+    out <= sum[(2*N)-2:N-1];
 
 // address counter
 
